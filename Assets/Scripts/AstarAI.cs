ef8f48e98eac0dbadb09d31d10cc48d75dac3df1
AsuoyGithub <aqfrswde@sjtu.edu.cn>
2024-06-09 00:08:25 +0800

    场景、自动寻路

---
+++ Assets/Scripts/AstarAI.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+using System;
+
+public class AstarAI : MonoBehaviour
+{
+    public GameObject targetObject;
+    private Vector3 targetPosition;
+    private Seeker seeker;
+    public Path path;
+    public float speed = 100.0f;
+    public float turnSpeed = 5f;
+    public float nextWaypointDistance = 3;
+    private int currentWaypoint = 0;
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        seeker.pathCallback += OnPathComplete;
+    }
+    void FixedUpdate()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 1.2f);
+        bool peopleNearby = false;
+        foreach (var hitCollider in hitColliders)
+        {
+            // 排除主角自己
+            if (hitCollider.gameObject != gameObject && hitCollider.gameObject.layer == LayerMask.NameToLayer("People"))
+            {
+                // 检查people对象与targetObject的距离
+                float peopleToTargetDistance = Vector3.Distance(hitCollider.transform.position, targetObject.transform.position);
+                float selfToTargetDistance = Vector3.Distance(transform.position, targetObject.transform.position);
+                if (peopleToTargetDistance < selfToTargetDistance)
+                {
+                    peopleNearby = true;
+                    break;
+                }
+            }
+        }
+        if (peopleNearby)
+        {
+            speed = 0;
+        }
+        else
+        {
+            speed = 5.0f;
+        }
+        targetPosition = targetObject.transform.position;
+        seeker.StartPath(transform.position, targetPosition);
+        if (path == null)
+        {
+            return;
+        }
+        // 当前搜索点编号大于等于路径存储的总点数时，路径搜索结束
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            Debug.Log("路径搜索结束");
+            return;
+        }
+        Vector3 dir = (path.vectorPath[currentWaypoint + 1] - transform.position);//.normalized;
+        dir *= speed * Time.fixedDeltaTime;
+        // 玩家转向
+        transform.Translate(Vector3.forward * Time.fixedDeltaTime * speed);
+        Quaternion targetRotation = Quaternion.LookRotation(dir);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
+        // 玩家当前位置与当前的航向点距离小于一个给定值后，转向下一个航向点
+        if (Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]) < nextWaypointDistance)
+        {
+            currentWaypoint++;
+            return;
+        }
+    }
+    /// <summary>
+    /// 当寻路结束后调用这个函数
+    /// </summary>
+    /// <param name="p"></param>
+    private void OnPathComplete(Path p)
+    {
+        Debug.Log("发现这个路线" + p.error);
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+    private void OnDisable()
+    {
+        seeker.pathCallback -= OnPathComplete;
+    }
+}
+
