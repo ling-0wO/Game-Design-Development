6081ef3988a2f410b16ff1cb487c8bb8b5c71c74
LAPTOP-2BT5JMFS\Ling <281660942@qq.com>
2024-06-15 17:39:23 +0800

    遇到危险切换状态以及切回fix bug

--- Assets/Scripts/Evacuation.cs
+++ Assets/Scripts/Evacuation.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DefaultNamespace;
 public class Evacuation : MonoBehaviour
 {
     public float maxVelocity;
@@ -23,7 +23,6 @@ public class Evacuation : MonoBehaviour
 
     // 计时器
     private float scriptRunningTime = 0f;
-
     void Start()
     {
         currentVelocity = Vector3.zero;
@@ -41,6 +40,8 @@ public class Evacuation : MonoBehaviour
         if (change == 1)
         {
             // animator 
+            HumanController humanController = gameObject.GetComponent<HumanController>();
+            humanController.SetState("Running");
             AstarAI.enabled = true;
             change = 0;
             Eva.enabled = false;
@@ -137,7 +138,10 @@ public class Evacuation : MonoBehaviour
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, 100f, dangerSourceLayer);
         // 超过范围了，人不再害怕
         if(hitColliders.Length == 0)
+        {
             change = 1;
+        }
+           
         
         foreach (var hitCollider in hitColliders)
         {
